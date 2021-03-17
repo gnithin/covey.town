a0e8b6df09973bf9bbf13a532582245043940447
Akshay Kurmi <akskurmi@gmail.com>
2021-03-17 11:48:12 -0400

    Add payload types for incoming and outgoing messages

--- services/roomService/src/types/payloads.ts
+++ services/roomService/src/types/payloads.ts
@@ -1,6 +1,20 @@
+import Player from "./Player";
+
 export interface JoinTownResponse {
   coveyUserID: string,
   coveySessionToken: string,
   providerVideoToken: string,
   providerRoomID: string
+}
+
+export interface IncommingChatMessage {
+  message: string;
+  broadcastRadius: number;
+}
+
+export interface OutgoingChatMessage {
+  sender: Player;
+  message: string;
+  timestamp: string;
+  receivingPlayers: Player[] | undefined;
 }

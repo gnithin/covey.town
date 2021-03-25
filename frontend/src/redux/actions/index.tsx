abb887cc44391d11feb88f5c04609a6ef1468c91
Nithin <nithin.linkin@gmail.com>
2021-03-25 10:13:09 -0400

    [redux] Add a dummy export to shut the linter up

--- frontend/src/redux/actions/index.tsx
+++ frontend/src/redux/actions/index.tsx
@@ -1,1 +1,3 @@
 export const ACTION_UPDATE_CURRENT_MESSAGE = "update_current_message";
+// TODO: Remove this. Added this just to appease the linter
+export const ACTION_ANOTHER_ACTION = "dummy-action";

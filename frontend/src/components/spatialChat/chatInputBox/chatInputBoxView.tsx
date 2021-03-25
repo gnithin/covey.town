a2972876e8c01a51e5053e9884205824efb48cda
Nithin <nithin.linkin@gmail.com>
2021-03-25 10:24:47 -0400

    [redux] Use the chat state

--- frontend/src/components/spatialChat/chatInputBox/chatInputBoxView.tsx
+++ frontend/src/components/spatialChat/chatInputBox/chatInputBoxView.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input } from '@chakra-ui/react'
 
-export const ChatInputBoxView: React.FunctionComponent = () =>
-(
-    <div>
-        Chat input box
-    </div>
-);
+export const ChatInputBoxView: React.FunctionComponent = () => {
+    const [chat, setChat] = useState("");
+    return (
+        <div className="chat-box-container">
+            <Input
+                placeholder="Say Something..."
+                size="lg"
+                value={chat}
+                onChange={(e) => {
+                    setChat(e.target.value);
+                }}
+            />
+        </div>
+    )
+};
 
 export default ChatInputBoxView;

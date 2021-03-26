2171f89a09ddea15d656e69f0372cd3d5e311d4c
wonghaobang <wonghaobang@gmail.com>
2021-03-26 10:35:22 -0400

    styled input box

--- frontend/src/components/spatialChat/chatInputBox/chatInputBoxView.tsx
+++ frontend/src/components/spatialChat/chatInputBox/chatInputBoxView.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Input } from '@chakra-ui/react'
+import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
 import IChatInputBoxView from './IChatInputBoxView';
 
 const ChatInputBoxView: React.FunctionComponent<IChatInputBoxView> = (
     { value, onInputChanged, onInputSubmit }: IChatInputBoxView) => (
     <div className="chat-box-container">
-        <Input
-            placeholder="Say Something..."
-            size="lg"
-            value={value}
-            onChange={(e) => {
-                onInputChanged(e.target.value);
-            }}
-            onKeyPress={async (e) => {
-                if (e.key === "Enter") {
-                    await onInputSubmit();
-                }
-            }}
-        />
+        <InputGroup size="md">
+            <Input
+                placeholder="Say Something..."
+                size="lg"
+                value={value}
+                onChange={(e) => {
+                    onInputChanged(e.target.value);
+                }}
+                onKeyPress={async (e) => {
+                    if (e.key === "Enter") {
+                        await onInputSubmit();
+                    }
+                }}
+            />
+            <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={onInputSubmit} colorScheme="teal">
+                    send
+                </Button>
+            </InputRightElement>
+        </InputGroup>
     </div>
 );
 
--- frontend/src/components/spatialChat/spatialChatContainer.tsx
+++ frontend/src/components/spatialChat/spatialChatContainer.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Box } from '@chakra-ui/react';
 import ConversationsList from './conversationsList';
 import ChatInputBox from './chatInputBox';
+
 
 export const SpatialChatContainer: React.FunctionComponent = () =>
 (
     <div>
         Spatial chat container
         <ConversationsList />
-        <ChatInputBox />
+        <Box pos="absolute" bottom="0" width="30%" height="75px">
+            <ChatInputBox />
+        </Box>
+        
     </div>
 );
 

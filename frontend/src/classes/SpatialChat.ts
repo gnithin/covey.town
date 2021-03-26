fbcfb8f01267e714903bc2a4c3997ef5ddc3893e
Nithin <nithin.linkin@gmail.com>
2021-03-25 23:29:12 -0400

    [chat] Add new models for chat-entries

---
+++ frontend/src/classes/SpatialChat.ts
@@ -0,0 +1,43 @@
+import Player, { ServerPlayer } from "./Player";
+
+
+export interface ServerChatEntry {
+    sender: ServerPlayer;
+    message: string;
+    timestamp: number;
+    receivingPlayers: ServerPlayer[] | undefined;
+}
+
+export class ChatEntry {
+    sender: Player;
+
+    message: string;
+
+    timestamp: number;
+
+    receivingPlayers?: Player[];
+
+    constructor(sender: Player, message: string, timestamp: number, receivingPlayers: Player[] | undefined) {
+        this.sender = sender;
+        this.message = message;
+        this.timestamp = timestamp;
+        this.receivingPlayers = receivingPlayers;
+    }
+
+    static fromServerChat(serverChatEntry: ServerChatEntry): ChatEntry {
+        const serverReceivingPlayers = serverChatEntry.receivingPlayers;
+        let receivingPlayers;
+        if (serverReceivingPlayers && serverReceivingPlayers.length > 0) {
+            receivingPlayers = serverReceivingPlayers.map((serverPlayer) =>
+                Player.fromServerPlayer(serverPlayer)
+            )
+        }
+
+        return new ChatEntry(
+            Player.fromServerPlayer(serverChatEntry.sender),
+            serverChatEntry.message,
+            serverChatEntry.timestamp,
+            receivingPlayers
+        );
+    }
+}

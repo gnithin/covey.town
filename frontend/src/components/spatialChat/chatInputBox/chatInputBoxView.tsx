2de13e43f41dcbc9dc02f79a1f76e16a5009de27
Nithin <nithin.linkin@gmail.com>
2021-03-29 20:32:10 -0400

    [styling-conversation-list] Add icons

--- frontend/src/components/spatialChat/chatInputBox/chatInputBoxView.tsx
+++ frontend/src/components/spatialChat/chatInputBox/chatInputBoxView.tsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import { Button, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
+import { ChatIcon } from '@chakra-ui/icons';
 import IChatInputBoxView from './IChatInputBoxView';
+
 
 const ChatInputBoxView: React.FunctionComponent<IChatInputBoxView> = (
     { value, onInputChanged, onInputSubmit }: IChatInputBoxView) => (
-    <div className="chat-box-container">
-        <InputGroup size="md">
-            <Input
-                style={{
-                    backgroundColor: "#FFF", // It's soo odd that it defaults to transparent :D
-                }}
-                placeholder="Say Something..."
-                size="lg"
-                value={value}
-                onChange={(e) => {
-                    onInputChanged(e.target.value);
-                }}
-                onKeyPress={async (e) => {
-                    if (e.key === "Enter") {
-                        await onInputSubmit();
-                    }
-                }}
-            />
-            <InputRightElement>
-                <Button size="sm" onClick={onInputSubmit} colorScheme="teal">
-                    send
-                </Button>
-            </InputRightElement>
-        </InputGroup>
+    <div className="chat-box-container" style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+    }}>
+        <Input
+            style={{
+                backgroundColor: "#FFF", // It's soo odd that it defaults to transparent :D
+            }}
+            placeholder="Say Something..."
+            size="lg"
+            value={value}
+            onChange={(e) => {
+                onInputChanged(e.target.value);
+            }}
+            onKeyPress={async (e) => {
+                if (e.key === "Enter") {
+                    await onInputSubmit();
+                }
+            }}
+        />
+        <Button
+            onClick={onInputSubmit}
+            size="lg"
+        >
+            <ChatIcon />
+        </Button>
     </div>
 );
 

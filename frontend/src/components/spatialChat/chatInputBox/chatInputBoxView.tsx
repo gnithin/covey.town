ef9d885551c9c18e5bcb340f5d8753a4d693654d
Nithin <nithin.linkin@gmail.com>
2021-04-03 12:25:00 -0400

    [rich-text] Add untested logic to convert between editors

--- frontend/src/components/spatialChat/chatInputBox/chatInputBoxView.tsx
+++ frontend/src/components/spatialChat/chatInputBox/chatInputBoxView.tsx
@@ -10,7 +10,35 @@ import { ChatEditorType } from '../../../classes/SpatialChat';
 
 const ChatInputBoxView: React.FunctionComponent<IChatInputBoxView> = (
     { onInputSubmit, chatEditorType }: IChatInputBoxView) => {
-    const [chatMessage, setChatMessage] = useState("");
+    const [chatMessage, setChatMessage] = useState("sample");
+    const prevEditorRef = useRef<ChatEditorType | null>(null);
+    const richTextEditorRef = useRef<ReactQuill>(null);
+
+    useEffect(() => {
+        if (prevEditorRef.current !== null) {
+            switch (chatEditorType) {
+                case ChatEditorType.DEFAULT_EDITOR:
+                    if (
+                        prevEditorRef.current === ChatEditorType.RICH_TEXT_EDITOR
+                        && richTextEditorRef.current !== null
+                    ) {
+                        // Perform conversion from rich-text to default editor
+                        const editor = richTextEditorRef.current.getEditor();
+                        setChatMessage(editor.getText());
+                    }
+                    break;
+
+                case ChatEditorType.RICH_TEXT_EDITOR:
+                    if (prevEditorRef.current === ChatEditorType.DEFAULT_EDITOR) {
+                        // No conversions required
+                    }
+                    break;
+
+                default:
+            }
+        }
+        prevEditorRef.current = chatEditorType
+    }, [chatEditorType]);
 
     const submitHandler = async () => {
         await onInputSubmit(chatMessage);
@@ -20,8 +48,8 @@ const ChatInputBoxView: React.FunctionComponent<IChatInputBoxView> = (
     return (
         <>
             <ReactQuill
-                className={Constants.CUSTOM_PRIORITY_FOCUS_CLASS_FOR_INPUT}
                 theme="snow"
+                ref={richTextEditorRef}
                 value={chatMessage}
                 onChange={setChatMessage}
             />
